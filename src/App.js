033e371a95b31462d4f9490cfb0cb8da3d4cec5d
Abhay Choudhary <abhay@Abhays-MacBook-Air.local>
2023-01-25 23:32:17 -0500

    object_hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
 
-function App() {
+//updating the state objects
+// updating the state of the star(favorite) so that 
+//when the user clicks, it changes 
+export default function App() {
+    const [contact, setContact] = React.useState({
+      firstName: "John",
+      lastName: "Doe",
+      phone: "+1 (719) 555-1212",
+      email: "itsmyrealname@example.com",
+      isFavorite: false
+  })
+  
+  let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png"
+  
+  function toggleFavorite() {
+      
+      setContact(prevContact => {   
+          return{                               
+              ...contact,           // here the whole object should be open, it's like copying all the elements of the
+              isFavorite : !prevContact.isFavorite //of the object and specifying in "," which one to change
+                                          //Also, notice as it is an object, it is wrapped around {} coz we are returning an object
+          }
+      })
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <main>
+          <article className="card">
+              <img src="./images/user.png" className="card--image" />
+              <div className="card--info">
+                  <img 
+                      src={`../images/${starIcon}`} 
+                      className="card--favorite"
+                      onClick={toggleFavorite}
+                  />
+                  <h2 className="card--name">
+                      {contact.firstName} {contact.lastName}
+                  </h2>
+                  <p className="card--contact">{contact.phone}</p>
+                  <p className="card--contact">{contact.email}</p>
+              </div>
+              
+          </article>
+      </main>
+  )
+  
 }
 
-export default App;
