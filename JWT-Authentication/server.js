567f96c03543b528d2a88fa7237780a78c7f82c7
Abhay Choudhary <abhaychoudhary804@gmail.com>
2023-03-08 00:20:01 -0500

    authentication middleware set

--- JWT-Authentication/server.js
+++ JWT-Authentication/server.js
@@ -19,8 +19,8 @@ const posts = [
 ]
 
 app.get('/posts', authenticateToken, (req,res) => {
-    res.json(posts)
-    //res.json(posts.filter(post => post.username === req.user.name))
+
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
 app.post('/login', (req,res) => {
