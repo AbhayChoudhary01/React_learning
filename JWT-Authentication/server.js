fecf1f8d50db51c8526aebdf2c842e9af87eca86
Abhay <abhaychoudhary804@gmail.com>
2023-03-10 21:13:23 -0500

    Authentication system completed

--- JWT-Authentication/server.js
+++ JWT-Authentication/server.js
@@ -4,45 +4,44 @@ require('dotenv').config()
 
 const jwt = require('jsonwebtoken')
 
-app.use(express.json())  //lets application use json from the body that gets passed upto it
+app.use(express.json())
+
 const posts = [
     {
-        username: 'kyle',
-        title: 'Post 1',
-        address: 'earth'
-    },
+        username: 'jim', 
+        title: 'post 2'
+    }, 
     {
-        username: 'Jim',
-        title: 'Post 2',
-        address: 'moon'
+        username: 'john', 
+        title: 'post 3'
     }
 ]
 
-app.get('/posts', authenticateToken, (req,res) => {
-
+app.get('/posts',authenticateToken, (req,res) => {
     res.json(posts.filter(post => post.username === req.user.name))
 })
 
 app.post('/login', (req,res) => {
-    //Authenticate user
-    const username = req.body.username
-    const user = {name: username}
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken })
+// Authenticate user
+const username = req.body.username
+const user = { name: username }
+const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+res.json({ accessToken: accessToken})
 })
 
 function authenticateToken(req,res,next){
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    // Bearer TOKEN
+    //Bearer TOKEN 
     if (token == null) return res.sendStatus(401)
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403)
+        if(err) return res.sendStatus(403)
         req.user = user
-        next()        //so that we can move on from our middle ware
+        next()
     })
-   
+    
+
 }
 
 app.listen(3000)
