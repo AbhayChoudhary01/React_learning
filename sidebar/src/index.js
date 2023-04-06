fee85c308fb23cafe43f2ba4c4d2968c10ef6c50
Abhay Choudhary <abhaychoudhary804@gmail.com>
2023-04-05 22:42:19 -0400

    new additions

--- sidebar/src/index.js
+++ sidebar/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Final from  './final'
 import Test from './test';
 
 
@@ -8,6 +8,7 @@ import Test from './test';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-        <Test /> 
+        {/* <Test />  */}
+        <Final/>
     </>
 );
